44e6643295512d8a5c12771506324fb6b62a9e28
Maather Mohammed ALRiyami <138444687+Maather99@users.noreply.github.com>
2023-07-31 15:57:46 +0400

    commit1

---
+++ sum of all values in the list.py
@@ -0,0 +1,15 @@
+MyList=[3, 5 ,1 ,7 ,2 ,8]
+sum=0
+
+for i in MyList:
+   
+    sum+= i
+print(" The total of",MyList ,"is: " ,sum)
+    
+## MyList=[3, 5 ,1 ,7 ,2 ,8]
+#sum=0
+
+#for i in range(len(MyList)):
+   
+    #sum+= MyList[i]
+#print(" The total of",MyList ,"is: " ,sum)
