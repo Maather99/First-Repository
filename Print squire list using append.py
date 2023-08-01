c793bdabb06be95d0866ed78bd1245038046e5fb
Maather Mohammed ALRiyami <138444687+Maather99@users.noreply.github.com>
2023-08-01 16:47:15 +0400

    Update and rename Prit sequare list using append.py to Print squire list using append.py

---
+++ Print squire list using append.py
@@ -0,0 +1,8 @@
+sequire=[]
+def SqList(n):
+    
+    for i in range(n):
+        sequire.append(i*i)
+    return sequire
+
+print(SqList(10))
--- Prit sequare list using append.py
+++
@@ -1,8 +0,0 @@
-sequare=[]
-def SqList(n):
-    
-    for i in range(n):
-        sequare.append(i*i)
-    return sequare
-
-print(SqList(10))
