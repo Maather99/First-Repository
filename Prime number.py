8315242db75ab68d46efa126c554b92aa34ee21f
Maather Mohammed ALRiyami <138444687+Maather99@users.noreply.github.com>
2023-08-01 16:39:57 +0400

    commit1

---
+++ Prime number.py
@@ -0,0 +1,10 @@
+
+def is_prime(num):
+    if num < 2:
+        return False
+    for i in range(2, int(num//2)+1):
+        if(num % i == 0):
+            return False
+    return True
+for num in range(1,31):
+    print(f"{num}|{is_prime(num)}")
