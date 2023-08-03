326938490cf51e60fa0057456d78bc334729412a
Maather Mohammed ALRiyami <138444687+Maather99@users.noreply.github.com>
2023-08-03 13:31:38 +0400

    commit1

---
+++ Read file and print thr first line.py
@@ -0,0 +1,18 @@
+input_file= open("input.txt", "r")
+
+#---------------------
+#to print the first line
+#line= input_file.readline()
+#print(line)
+
+#----------------------
+#to print the file
+#print(input_file.read())
+
+
+#-----------------------
+# to print the even number only
+#for line in input_file:
+ #   if int(line)%2 ==0:
+  #      print(line)
+
